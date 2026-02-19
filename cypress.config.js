8c7d7a669506a6fc81111b962cbdc2aeab8d1553
mohit03nandan <mohitnandan81825@gmail.com>
2026-02-19 15:34:59 +0530

    automate/BrandPage

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,15 @@ const { defineConfig } = require("cypress");
 const fs = require("fs");
 
 function getConfigurationByFile(file) {
-  const pathToConfigFile = `config/${file}.json`;
+  const pathToConfigFile = `cypress/config/${file}.json`;
   return JSON.parse(fs.readFileSync(pathToConfigFile));
 }
 
 module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.cy.js",
-
+    viewportWidth: 1400,
+    viewportHeight: 900,
     setupNodeEvents(on, config) {
       const file = config.env.configFile || "staging";
 
